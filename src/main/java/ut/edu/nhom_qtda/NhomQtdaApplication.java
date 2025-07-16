c354c5edcc59c6de2eb476a5787a75b30b91b429
Gialac2006 <gialacnguyentran@gmail.com>
2025-07-17 01:10:00 +0700

    first commit

---
+++ src/main/java/ut/edu/nhom_qtda/NhomQtdaApplication.java
@@ -0,0 +1,13 @@
+package ut.edu.nhom_qtda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhomQtdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NhomQtdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ut/edu/nhom_qtda/NhomQtdaApplicationTests.java
@@ -0,0 +1,13 @@
+package ut.edu.nhom_qtda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhomQtdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
